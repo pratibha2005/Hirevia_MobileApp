79c9b4b353968783a85a37245293c7d7a377fd70
Pratibha Panda <pratibhaa2005@gmail.com>
2026-04-05 23:15:58 +0530

    website deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,12 @@ const MONGODB_URI = process.env.MONGODB_URI || '';
 console.log('\n🚀 Starting Hirevia Backend...\n');
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "https://hirevia-4bioyprpv-pratibha-portfolio.vercel.app/",
+  credentials: true
+}));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
